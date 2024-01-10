7495c3c23417dd594a2d931cb65d8778a1ddc88a
Erik Silcox <eriksilcox651@gmail.com>
2024-01-09 20:47:33 -0600

    added card function

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,22 @@
+// to use State we need Hooks
+// Hooks small bits of behavior
+// {} = all of State
+import { useState } from 'react';
+
+
 function Card() {
+  const [myName, setMyName] = useState('Myron');
+
+  function clickChangeName() {
+    //set name to new name
+    console.log('test');
+    setMyName('MYRON');
+  }
     return (
         <div className="card">
-        <button>
-          count is 0
-        </button>
+        <button>count is 0</button>
+        <p>{myName}</p>
+        <button onClick={clickChangeName}>Change Name</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,3 @@
-
 import reactLogo from '../../assets/react.svg';
 import viteLogo from '/vite.svg';
 
@@ -7,6 +6,7 @@ import viteLogo from '/vite.svg';
 
 function Header() {
     return (
+    <>
         <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -15,11 +15,11 @@ function Header() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
 
-        <h1>Vite + React</h1>
+        
       </div>
-
-
+      <h1>Vite + React</h1>
+      </>
     );
-};
+}
 
 export default Header;

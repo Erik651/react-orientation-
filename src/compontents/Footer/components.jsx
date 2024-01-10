bbbcb067993a78d82ec34b7471d3142d24bc5567
Erik Silcox <eriksilcox651@gmail.com>
2024-01-09 19:38:06 -0600

    test

---
+++ src/compontents/Footer/components.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+        Myron APP!!!
+      </p> 
+
+    );
+}
+
+export default Footer;

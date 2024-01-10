103f6671a3d260a6d9b33cd5713733642ec173e9
Erik Silcox <eriksilcox651@gmail.com>
2024-01-09 19:52:45 -0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Card from './components/Card'
-import Footer from './components/Footer/Footer'
-import './App.css'
+
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import Card from './components/Card/Card';
+import Footer from './components/Footer/Footer';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
